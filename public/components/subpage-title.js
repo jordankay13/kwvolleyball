4eb3b017b6f5a9c5261106da1a41da5d97f2fe2f
Jordan Kay <Jordan.Kay@D2L.com>
2025-12-20 10:17:49 -0500

    * Move components into ./components/ * Introduce subpage-title component * Load team-table data from json * Remove table layout in division pages * Add basic responsive css to division pages

---
+++ public/components/subpage-title.js
@@ -0,0 +1,45 @@
+class SubpageTitle extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" });
+        const title = this.getAttribute("data-title");
+        if (!title) {
+            console.error("data-title not specified for SubpageTitle");
+            return;
+        }
+        const div = document.createElement("div");
+        div.style.backgroundImage = "url('images9054COB9.gif')";
+        div.style.textAlign = "center";
+        div.style.fontFamily = 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif';
+        const websiteTitle = document.createElement("span");
+        websiteTitle.style.fontWeight = "bold";
+        websiteTitle.style.fontSize = "xx-large";
+        websiteTitle.style.fontStyle = "italic";
+        websiteTitle.textContent = "Kitchener Co-ed Volleyball League";
+        const homeLink = document.createElement("a");
+        homeLink.style.fontWeight = "bolder";
+        homeLink.style.fontStyle = "italic";
+        homeLink.style.fontSize = "medium";
+        homeLink.href = "./index.htm";
+        homeLink.textContent = "home";
+
+        const divisionTitle = document.createElement("span");
+        divisionTitle.textContent = title;
+        divisionTitle.style.fontStyle = "italic";
+        divisionTitle.style.fontSize = "xx-large";
+
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
+        div.appendChild(websiteTitle);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
+        div.appendChild(homeLink);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(divisionTitle);
+        shadow.appendChild(div);
+    }
+}
+
+customElements.define("subpage-title", SubpageTitle);
